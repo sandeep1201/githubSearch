df8b339d0d38679913191c257b652bd13b9a7ffe
Alalla, SandeepReddy <SALALLA@amfam.com>
2017-01-18 09:29:51 -0600

    initial commit

--- app/services/github.service.ts
+++ app/services/github.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angualar/core';
+import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 
 import 'rxjs/add/operator/map';
@@ -11,12 +11,20 @@ export class GithubService{
 	private client_secret = 'f2fdba1b8c6a418efe0ce3129d5f187feca1fdb1';
 
 	constructor(private _http:Http){
-		console.log('Guthub service ready...');
+		console.log('Github service ready...');
 		this.username = 'sandeep1201';
 	}
 	getUser(){
-		return this._http.get('http://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret'+this.client_secret)
+		return this._http.get('http://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
 		   .map(res=>res.json());
 
+	}
+	getRepos(){
+		return this._http.get('http://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_secret)
+		   .map(res=>res.json());
+
+	}
+	updateUser(username:string){
+		this.username = username;
 	}
 }
